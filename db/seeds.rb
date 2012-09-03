ddc72d27387423c3902eca4fe844193766139b27
Eduardo Reche <edu.reche@gmail.com>
2012-09-03 20:25:33 -0300

    Devise configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Eduardo', email: 'edu.reche@gmail.com', password: 'aaa123')
