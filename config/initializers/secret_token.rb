e355137b691ff6dffc27c7ffa8adfbda30badf20
Eduardo Reche <edu.reche@gmail.com>
2012-09-03 19:50:16 -0300

    Initial commit, test gems configured

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeroTudo::Application.config.secret_token = '805757f4037a883d73956d3ed290755d0ade31dc7cf607898484da4a6d2286bc26f4663f91723e1a9e8d54af0e0c6115a1e5e48c9a517d3d664998e571ab9947'
