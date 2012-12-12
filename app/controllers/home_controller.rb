07e9867915d921fd51998aeaa7a6fdc801d896fe
Eduardo Reche <edu.reche@gmail.com>
2012-12-12 20:26:30 -0200

    - Added dashboard functions to home controller - Removed exclusive Dashboard controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
+  
   def index
+    next_events if user_signed_in? 
   end
+  
+  private
+  def next_events
+    today = Date.today
+    limit = today + 15
+    query = build_query(today,limit)
+
+    @celebrations = Celebration.where(query)
+  end
+  
+  def build_query(today, limit)
+     query = "(month = #{today.month} and day between #{today.day} "
+    
+     if( today.month == limit.month )
+       query += " and #{limit.day})"
+     else
+       query += " and #{Time.days_in_month(today.month, today.year)}) or "+
+                    " (month = #{limit.month} and day between 1 and #{limit.day})"
+     end
+  end
+  
 end
