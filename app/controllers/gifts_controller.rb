6d27e5dbacc70ef38e183c421141090b1f56d26d
Eduardo Reche <edu.reche@gmail.com>
2013-02-19 23:20:31 -0300

    Added gifts filters

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,9 @@
 class GiftsController < ApplicationController
   def index
     @products = Product.order('created_at desc').page(params[:page]).per(12)
+    
+    @tags = Tag.order(:name)
+    
+    @groups = Group.order(:name)
   end
 end
